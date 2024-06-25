32375c736e3bb24664efc3b710cdc1781268a9a9
Vladislav Adamets <adametsderschopfer@gmail.com>
2024-06-26 00:11:03 +0200

    feat(db): implement store interface and config file

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,14 +1,18 @@
 package apiserver
 
+import "github.com/adametsderschopfer/http-rest-api_go-edu/internal/store"
+
 // Config ...
 type Config struct {
 	BindAddress string `toml:"bind_address"`
 	LogLevel    string `toml:"log_level"`
+	Store       *store.Config
 }
 
 func NewConfig() *Config {
 	return &Config{
 		BindAddress: ":8080",
 		LogLevel:    "debug",
+		Store:       store.NewConfig(),
 	}
 }
